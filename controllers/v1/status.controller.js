aff10754e4b0f66b779d22a8879ef5253c61fafb
khwan <rudghks7816@gmail.com>
2020-03-28 17:37:02 +0900

    feat: api 컨트롤러 수정
    목적에 맞게 api 라우트, 컨트롤러 수정

--- controllers/v1/status.controller.js
+++ controllers/v1/status.controller.js
@@ -30,19 +30,4 @@ const sample = (req, res, next) => {
   res.json(mock)
 }
 
-const company = async (req, res, next) => {
-  console.time('123')
-  const key = SERVICE_KEY
-  const { data } = await axios.get(`http://apis.data.go.kr/B552015/NpsBplcInfoInqireService/getBassInfoSearch?serviceKey=${key}`, {
-    params: {
-      wkpl_nm: '그립랩스',
-      pageNo: 1,
-      numOfRows: 10
-    }
-  })
-  console.timeEnd('123')
-
-  return res.json(data)
-}
-
-export { sample, getIndex, company }
+export { sample, getIndex }
--- routes/v1/status.route.js
+++ routes/v1/status.route.js
@@ -1,12 +1,10 @@
 import express from 'express'
 const router = express.Router()
 
-import { getIndex, sample, company } from '../../controllers/v1/status.controller'
+import { getIndex, sample } from '../../controllers/v1/status.controller'
 
 router.get('/', getIndex)
 
 router.get('/sample', sample)
-
-router.get('/company', company)
 
 export default router
