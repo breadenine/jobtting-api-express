61ad4c0f07150fd20da5d46dff0a4e37bb312b73
khwan <rudghks7816@gmail.com>
2020-04-18 15:48:26 +0900

    feat: config를 클래스로 변경

--- config/index.js
+++ config/index.js
@@ -1,5 +1,15 @@
 require('dotenv').config()
 
-export default {
-  SERVICE_KEY: process.env.SERVICE_KEY
+const debug = require('debug')('jobtting-api-express:server')
+
+export default class Config {
+  constructor() {}
+
+  static get debug() {
+    return debug
+  }
+
+  static get SERVICE_KEY() {
+    return process.env.SERVICE_KEY
+  }
 }
